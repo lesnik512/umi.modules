ac31d20dff42747875ff4dea1e6a375d62a05cb7
lesnik <lesnik512@gmail.com>
2019-05-22 16:28:53 +0300

    + config for umimarket

---
+++ developerTools/modulePacker/umimeta_config.php
@@ -0,0 +1,19 @@
+<?php
+    return [
+        'package' => 'shiriev_meta',
+        'destination' => './output',
+        'directories' => [
+            './classes/components/meta',
+            './classes/modules/meta',
+            './styles/skins/mac/data/modules/meta',
+            './styles/skins/modern/data/modules/meta',
+            './man/ru/meta/',
+        ],
+        'files' => [
+            './images/cms/admin/mac/icons/big/meta.png',
+            './images/cms/admin/mac/icons/medium/meta.png',
+            './images/cms/admin/mac/icons/small/meta.png',
+            './images/cms/admin/modern/icon/meta.png',
+        ]
+    ];
+?>
